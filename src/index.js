98bb57799c9049e10a8f17a53c70ada35b348489
Miki22986 <elisa.miki@gmail.com>
2025-03-05 21:53:00 +0100

    add javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 function generatePoem(event){
     event.preventDefault();
     let text_imput = document.querySelector("#text_input");
-    let poem = document.querySelector("#poem");
-    poem.innerHTML = text_imput.value;
-
+    let text = text_imput.value;
+    new Typewriter('#poem', {
+        strings: `${text}`,
+        autoStart: true,
+        cursor: " ",
+        });
 }
 
 let form_element = document.querySelector("#form_elements");
-form_element.addEventListener("submit", generatePoem)
+form_element.addEventListener("submit", generatePoem);
