9510e0c2bd13b43b9fcb1f237dccbbddc99f2fc0
Miki22986 <elisa.miki@gmail.com>
2025-03-05 22:04:59 +0100

    collegarmento ai

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function write_poem(response){
+    
+}
+
+function all_ai(){
+    let api_key = "8bb47a1aat7f98e60875f1b93o1d6a06";
+    let prompt = "Write a short rhyming poem";
+    let context = "Be fun and concise. No more than 6 verses";
+    let Ai_link  = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
+    axios.get(Ai_link).then(write_poem);
+}
+
 function generatePoem(event){
     event.preventDefault();
     let text_imput = document.querySelector("#text_input");
