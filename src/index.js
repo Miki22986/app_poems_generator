21dc4f2a824766dc27c39ffc711900fb01d21e42
Miki22986 <elisa.miki@gmail.com>
2025-03-06 16:53:34 +0100

    controlli su javascript

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ function write_poem(response){
 function call_ai(){
     let text_imput = document.querySelector("#text_input");
     let text = text_imput.value;
-    let api_key = "8bb47a1aat7f98e60875f1b93o1d6a06";
-    let prompt = `Write a short rhyming poem ${text} and separate each line, except the last one, with a <br>`;
-    let context = "Be fun and concise. No more than 6 verses";
-    let Ai_link  = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
-    axios.get(Ai_link).then(write_poem);
+    if ((text !== "")&&(text!==" ")){
+        let api_key = "8bb47a1aat7f98e60875f1b93o1d6a06";
+        let prompt = `Write a short rhyming poem ${text} and separate each line, except the last one, with a <br>`;
+        let context = "Be fun and concise. No more than 6 verses";
+        let Ai_link  = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
+        axios.get(Ai_link).then(write_poem);
+    } else {
+        let poem_space =document.querySelector("#poem");
+        poem_space.innerHTML = "You must submit a word";
+    }
 }
 
 function generatePoem(event){
