57396349bd617cf28bc4bb859dccfb5fc3249ea6
Miki22986 <elisa.miki@gmail.com>
2025-03-06 22:46:06 +0100

    final touch

--- src/index.js
+++ src/index.js
@@ -8,15 +8,19 @@ function write_poem(response){
 
 function call_ai(){
     let text_imput = document.querySelector("#text_input");
+    let poem_space =document.querySelector("#poem");
+
     let text = text_imput.value;
-    if (text!==" "){
+    if (text!=="  "){
         let api_key = "8bb47a1aat7f98e60875f1b93o1d6a06";
         let prompt = `Write a short rhyming poem ${text} and separate each line, except the last one, with a <br>.`;
         let context = "Be fun and concise. No more than 6 verses";
         let Ai_link  = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
+
+        poem_space.innerHTML = `<div> <span class="rotate">⏳ </span> Generating a poem about ${text}</div>`;
         axios.get(Ai_link).then(write_poem);
     } else {
-        let poem_space =document.querySelector("#poem");
+        
         poem_space.innerHTML = "<p>You must submit a word</p>";
     }
 }
